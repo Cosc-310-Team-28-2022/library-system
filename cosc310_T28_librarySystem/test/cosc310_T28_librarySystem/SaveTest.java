f7c9705f2d9b031004db071c9115020cfcc11319
eztang00 <eztang00@gmail.com>
2022-11-14 19:23:50 -0800

    save test better

--- cosc310_T28_librarySystem/test/cosc310_T28_librarySystem/SaveTest.java
+++ cosc310_T28_librarySystem/test/cosc310_T28_librarySystem/SaveTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,13 +15,14 @@ class SaveTest {
     @Test
     void test() {
 	LocalLibraryData savedLibraryData = new LocalLibraryData();
-	savedLibraryData.bookList.add(new Book());
+	savedLibraryData.bookList.add(new Book(new BookGroup(), 1234567890L, LocalDate.MIN, 123, "Test Book", "Test Author", "Test Category", false));
 
 	UserAndManagerTerminal userAndManagerTerminal = new UserAndManagerTerminal();
 	userAndManagerTerminal.saveSession(null, savedLibraryData, "save_test_1.ser");
 
 	LocalLibraryData loadedLibraryData = userAndManagerTerminal.loadSession("save_test_1.ser");
 	LocalLibraryData differentLibraryData = new LocalLibraryData();
+	differentLibraryData.bookList.add(new Book(new BookGroup(), 1234567890L, LocalDate.MIN, 123, "Test Different Book", "Test Author", "Test Category", false));
 
 	assertEquals(savedLibraryData, loadedLibraryData);
 	assertNotEquals(savedLibraryData, differentLibraryData);
